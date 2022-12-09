c85e640885753a705e381cf658cb943dff697f11
sonhoang205 <hson20052001@gmail.com>
2022-12-09 17:19:10 +0700

    commit 9/12

--- BE/modules/Task/task.router.js
+++ BE/modules/Task/task.router.js
@@ -4,9 +4,9 @@ const needAuthenticated = require('../../middlewares/needAuthenticated');
 
 const taskController = require('./task.controller');
 
-router.post('/create', taskController.createTask);
+router.post('/create',needAuthenticated, taskController.createTask);
 router.get('/:taskId', needAuthenticated, taskController.getTask);
-router.get('/', taskController.getTasks);
+router.get('/',needAuthenticated, taskController.getTasks);
 router.put('/:taskId', needAuthenticated, taskController.updateTask);
 router.put('/updatestatus', needAuthenticated, taskController.updateStatusTask);
 router.delete('/:taskId', needAuthenticated, taskController.deleteTask);
