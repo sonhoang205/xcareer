605964f2aadfc0b1ca68e216428dbb848f532214
sonhoang205 <hson20052001@gmail.com>
2022-12-15 00:39:59 +0700

    commit 14/12

--- BE/modules/member/member.router.js
+++ BE/modules/member/member.router.js
@@ -6,9 +6,9 @@ const memberController = require('./member.controller');
 
 router.post('/add', memberController.addMember);
 router.delete('/kick', memberController.kickMember);
-router.get('/:projectId', memberController.getMembers);
+router.get('/projectId/:projectId', memberController.getMembers);
 router.get('/', memberController.getMember);
-// router.get('/:userId', memberController.getProjectsMember);
+router.get('/userId/:userId', memberController.getProjectsMember);
 
 // router.get('/', needAuthenticated, taskController.getTasks);
 // router.put('/:taskId', needAuthenticated, taskController.updateTask);
